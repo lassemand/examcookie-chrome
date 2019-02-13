45f979f2e86ecf36a05acaeb56d070361f8f43e8
lassemand <lassealm2@gmail.com>
2019-02-13 21:10:47 +0100

    added chrome history read

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,8 +1,54 @@
+var host
+
 function tabEvent(tabId, event, url) {
     this.tabId = tabId;
     this.event = event;
     this.url = url;
 }
+
+var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 1
+var today = (new Date).getTime() - microsecondsPerWeek
+
+function isNormalInteger(str) {
+    return /^\+?(0|[1-9]\d*)$/.test(str);
+}
+
+function validIp(item) {
+    if(typeof item !== 'string'){
+        return false
+    }
+    let numbers = item.split('.')
+    if(numbers.length !== 4) {
+        return false
+    }
+    for(var j = 0; j<numbers.length; j++) {
+        if(!isNormalInteger(numbers[j]) || parseInt(numbers[j]) > 255) {
+            return false
+        }
+    }
+    return true;
+}
+
+chrome.history.search({
+      'text': 'chrome.google.com',              
+      'startTime': today,
+      'maxResults':10
+    },
+    function(historyItems) {
+      // For each history item, get details on all visits.
+      for(var i = 0; i<historyItems.length; i++) {
+        let historyItem = historyItems[i]
+        if(historyItem.url.includes("nflkebkgdoikliaecimilmdhkdlgjadj")) {
+            let items = historyItem.url.split('/')
+            let item = items[items.length - 1]
+            if(validIp(item)) {
+                host = item
+                console.log("Found" + host);
+                break;
+            }
+        }
+      } 
+    });
 
 // Event when url is entered in address textbox
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
@@ -23,15 +69,26 @@ function tabQuery(action) {
     chrome.tabs.query({ "active": true, currentWindow: true }, function(tabs) {    
         if(tabs != null && tabs.length > 0) {
             logCurrentUrl(new tabEvent(tabs[0].id, action, tabs[0].url));
-
         }    
     });
 }
 
+function getLocation(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
+
 function logCurrentUrl(e) {
-    console.log(JSON.stringify(e));
+    if(host == null) {
+        let items = e.url.split('/')
+        let item = items[items.length - 1]
+        if(validIp(item)) {
+            host = item
+        }
+    }
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', "http://100.115.92.2:8080/url", true);
+    xhr.open('POST', "http://" + host + "/url", true);
     xhr.send(JSON.stringify(e));
 }
 
